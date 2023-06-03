de32876e0be4fb3950a0948741da0222ac1cda12
mjh316 <justinhuang316@gmail.com>
2023-06-03 16:43:25 -0700

    added input page

--- src/routes/info/infoform.svelte
+++ src/routes/info/infoform.svelte
@@ -1,32 +1,30 @@
 <script lang="ts">
-	let title = "ENVIRO";
+	let title = 'ENVIRO';
 </script>
 
 <div class="navbar">
 	<ul>
-        <li id="title">ENVIRO</li>
-        <li><a href="#">Go To Suggester Engine</a></li>
-        <li>About</li>
+		<li id="title">ENVIRO</li>
+		<li><a href="#">Go To Suggester Engine</a></li>
+		<li>About</li>
 	</ul>
 </div>
 
-
 <style>
-    .navbar {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333333;
-    }
+	.navbar {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #333333;
+	}
 
-    .navbar li {
-    float: left;
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 16px;
-    text-decoration: none;
-    }
-
+	.navbar li {
+		float: left;
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 16px;
+		text-decoration: none;
+	}
 </style>
---
+++ src/routes/input/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { city } from '@/stores/home';
+</script>
+
+<div class="flex flex-col items-center justify-center flex-1 bg-base-200">
+	<div class="form-control gap-y-2 w-full max-w-xs">
+		<label class="label" for="cityInput">
+			<span class="label-text">City Name</span>
+		</label>
+		<span id="cityInput" class="input flex flex-col justify-center input-bordered w-full max-w-xs">
+			{$city}
+		</span>
+		<label class="label" for="budgetInput">
+			<span class="label-text">Budget (in USD)</span>
+		</label>
+		<input
+			id="budgetInput"
+			type="number"
+			placeholder="Type here..."
+			class="input input-bordered w-full max-w-xs"
+		/>
+		<label class="label" for="timeInput">
+			<span class="label-text">Amount of Time You Will Spend</span>
+		</label>
+		<input
+			id="timeInput"
+			type="text"
+			placeholder="Type here..."
+			class="input input-bordered w-full max-w-xs"
+		/>
+	</div>
+</div>
