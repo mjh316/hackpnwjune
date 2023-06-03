fa65f2b32def90999f7cdf3a3d292303a46b7076
mjh316 <justinhuang316@gmail.com>
2023-06-03 07:34:37 -0700

    Pushed boilerplate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1 class="lg:text-5xl md:text-4xl text-3xl text-center text-green-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let title = 'Survace';
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="lg:text-5xl text-3xl md:text-4xl font-bold">
+				{title}
+			</h1>
+			<p class="py-6">
+				The premier survey platform for the modern web. Engage with your audience, future or
+				current. Get valuable insights and make better decisions.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
