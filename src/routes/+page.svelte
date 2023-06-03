480c163c39bd7a40329d43b878165e163c122715
mjh316 <justinhuang316@gmail.com>
2023-06-03 07:23:24 -0700

    Installed Tailwind!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="lg:text-5xl md:text-4xl text-3xl text-center text-green-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
