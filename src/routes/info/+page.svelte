68a625faeb8d7c189813f7d4a0e12fc56efee13c
shreyaanpathak <33208813+shreyaanpathak@users.noreply.github.com>
2023-06-04 12:15:20 -0700

    i love webdesign 😥

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import InfoForm from './infoform.svelte';
 </script>
-
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link href="https://fonts.googleapis.com/css2?family=Rambla&display=swap" rel="stylesheet" />
+</svelte:head>
 <div class="infocontainer flex h-full w-screen overflow-hidden">
 	<InfoForm />
 </div>
@@ -9,5 +13,8 @@
 <style>
 	.infocontainer {
 		background-color: #ffe5b7;
+	}
+	div {
+		font-family: 'Rambla', sans-serif;
 	}
 </style>
